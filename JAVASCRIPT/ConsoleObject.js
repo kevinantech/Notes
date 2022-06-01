9e3188db21cc506a1f21ecc59c9c11fa3f3ce826
Kevin Andres Gomez <kevinandresgom19@gmail.com>
2022-06-01 02:47:39 +0000

    Console object added

---
+++ JAVASCRIPT/ConsoleObject.js
@@ -0,0 +1,27 @@
+    // Nota: Los metodos de esta seccion son utiles para el desarrollador con el fin de gestionar tareas relacionadas a las funcionalidades que se estan implementado.
+    // FUNCIONES DE REGISTRO
+    //console.clear();    // --> Limpia la consola
+    //console.error("Error"); // --> Muestra un mensaje de error personalizado.
+    //console.info("Info");   // --> Muestra un mensaje informativo personalizado, a diferencia de console.log() este metodo no es depurativo.
+    //console.warn("Warn");   // --> Muestra un mensaje de advertencia personalizado.
+    /*
+        let vocales = ['A', 'E', 'I', 'O', 'U'];
+        console.table(vocales); // --> En el primer parametro recibe un objeto y  a raiz de este lo muestra en una tabla. 
+    */
+    // CONTEO
+    //console.count();    // --> Retorna por consola el numero de veces que se ha llamado esta funcion anteriormente,muy util para conteos.
+    //console.countReset();  // --> Resetea el numero de veces que se ha llamado la funcion.
+    // METODOS DE AGRUPACION
+    /*
+        console.group("Group1"); // --> Organiza en grupos varias instruciones en consola, aqui se inica un grupo.
+        console.log("Hello World"); // --> Esta instruccion va dentro del grupo Group1.
+        console.groupEnd(); // --> Cierra el grupo declarado mas cercano.
+        //console.groupCollapsed("Group2");   // --> Funciona igual que console.group() con la diferencia que el grupo definido no esta abierto por defecto al interactuar por consola.
+    */
+    // TEMPORIZADOR
+    //console.time(); // --> Inicia un contador
+    //console.timeLog();  // --> Muestra por consola la cantidad de tiempo desde que se inicio el contador hasta que se emplea este metodo.
+    //console.timeEnd();  // --> Finaliza el contador y muestra por consola la cantidad de tiempo transcurrido desde que se inicializo el contador.
+    // ESTILOS POR CONSOLA
+    //console.log("%cHola Mundo", "color: white; background-color: green; padding: 16px;");   // --> Es necesario recordar "%c" al inicio de la cadena a implementar, seguido de un segundo parametro especificando algunos estilos permitidos.
+    
