927bf8b1d43b3eec28247fbbd49e06045099f572
Kevin Andres Gomez <kevinandresgom19@gmail.com>
2022-06-03 16:58:57 +0000

    DB creation whit MySQL added

---
+++ MySQL.sql
@@ -0,0 +1,6 @@
+# CREACION DE BASE DE DATOS
+show databases;	# --> Visualiza las bases de datos.
+#create database Tienda;	# --> Crea la BD Tienda
+use Tienda;	# --> Seleccionamos la BD Tienda
+#drop database Test;	# --> Eliminamos la BD Test
+# CREACION DE TABLAS
