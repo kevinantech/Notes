e1b2671caa093d5148e3ba6348af06f30606cf69
Kevin Andres Gomez <kevinandresgom19@gmail.com>
2022-06-04 14:53:04 +0000

    rm MySQL, out of parent directory

--- MySQL.sql
+++
@@ -1,6 +0,0 @@
-# CREACION DE BASE DE DATOS
-show databases;	# --> Visualiza las bases de datos.
-#create database Tienda;	# --> Crea la BD Tienda
-use Tienda;	# --> Seleccionamos la BD Tienda
-#drop database Test;	# --> Eliminamos la BD Test
-# CREACION DE TABLAS
