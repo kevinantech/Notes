b72373d50521d5c3d5df0fc9a812c4b1440d9b22
Kevin Andres Gomez <kevinandresgom19@gmail.com>
2022-06-05 20:19:04 +0000

    Update repo

--- JAVA/GUI/src/GUI_JFRAME/Main.java
+++
@@ -1,8 +0,0 @@
-package GUI_JFRAME;
-public class Main {
-    public static void main(String[] args) {
-        HomeWindow homeWindow = new HomeWindow(); // --> Instanciamos a partir de nuestra plantilla HomeWIndow
-        homeWindow.setVisible(true); // --> Permitimos que la ventana sea visualizada mediante este metodo con el valor true.
-        
-    }
-}
--- MySQL/MySQL.sql
+++
@@ -1,5 +0,0 @@
-# CREACION DE BASE DE DATOS
-show databases;	# --> Visualiza las bases de datos.
-#create database Test;	# --> Crea la BD Test
-use Test;	# --> Seleccionamos la BD Test
-#drop database Test;	# --> Eliminamos la BD Test
